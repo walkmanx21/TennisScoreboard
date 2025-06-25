5667566092f398c869ef701b64b6b21998eba63f
walkmanx21 <walkmanx21@gmail.com>
2025-06-25 13:30:29 +0300

    Настроен подсчет геймов. Покрыто тестами

--- src/main/java/util/ScoreCalculationUtil.java
+++ src/main/java/util/ScoreCalculationUtil.java
@@ -3,10 +3,11 @@ package util;
 import model.Match;
 import model.Player;
 
+import javax.print.attribute.HashDocAttributeSet;
+
 public final class ScoreCalculationUtil {
 
-    private ScoreCalculationUtil() {
-    }
+    private ScoreCalculationUtil() {}
 
     public static void scoreCalculation(Integer playerWinPointId, Match match) {
         Player firstPlayer = match.getFirstPlayer();
@@ -21,24 +22,41 @@ public final class ScoreCalculationUtil {
         }
     }
 
+    private static void setsCalculation(Player winnerPlayer, Player loosingPlayer) {
+        int winnerPlayerSets = winnerPlayer.getPlayerSets();
+        int loosingPlayerSets = loosingPlayer.getPlayerSets();
+
+        if (winnerPlayerSets + loosingPlayerSets < 3) {
+            gamesCalculation(winnerPlayer, loosingPlayer);
+        }
+    }
+
     private static void gamesCalculation(Player winnerPlayer, Player loosingPlayer) {
         int winnerPlayerGames = winnerPlayer.getPlayerGames();
         int loosingPlayerGames = loosingPlayer.getPlayerGames();
-        int winnerPlayerSets = winnerPlayer.getPlayerSets();
+
         if (winnerPlayerGames <= 5) {
             pointsCalculation(winnerPlayer, loosingPlayer);
             if (winnerPlayer.getPlayerGames() - loosingPlayerGames >= 2 && winnerPlayer.getPlayerGames() == 6) {
-                winnerPlayer.setPlayerGames(0);
-                loosingPlayer.setPlayerGames(0);
-                winnerPlayer.setPlayerSets(winnerPlayerSets + 1);
+                setIsWon(winnerPlayer, loosingPlayer);
             }
+        }
+
+        if (winnerPlayerGames == 6 && loosingPlayerGames == 5) {
+            pointsCalculation(winnerPlayer, loosingPlayer);
+            if (winnerPlayer.getPlayerGames() == 7) {
+                setIsWon(winnerPlayer, loosingPlayer);
+            }
+        }
+
+        if (winnerPlayerGames == 6 && loosingPlayerGames == 6) {
+            tiebreak(winnerPlayer, loosingPlayer);
         }
     }
 
     private static void pointsCalculation(Player winnerPlayer, Player loosingPlayer) {
         String winnerPlayerPoints = winnerPlayer.getPlayerPoints();
         String loosingPlayerPoints = loosingPlayer.getPlayerPoints();
-        int winnerPlayerGames = winnerPlayer.getPlayerGames();
 
         switch (winnerPlayerPoints) {
             case ("0"):
@@ -52,18 +70,14 @@ public final class ScoreCalculationUtil {
                 break;
             case ("40"):
                 if (!loosingPlayerPoints.equals("40")) {
-                    winnerPlayer.setPlayerGames(winnerPlayerGames + 1);
-                    winnerPlayer.setPlayerPoints("0");
-                    loosingPlayer.setPlayerPoints("0");
+                    gameIsWon(winnerPlayer, loosingPlayer);
                 } else {
                     winnerPlayer.setPlayerPoints("AD");
                     loosingPlayer.setPlayerPoints("");
                 }
                 break;
             case ("AD"):
-                winnerPlayer.setPlayerGames(winnerPlayerGames + 1);
-                winnerPlayer.setPlayerPoints("0");
-                loosingPlayer.setPlayerPoints("0");
+                gameIsWon(winnerPlayer, loosingPlayer);
                 break;
             case (""):
                 winnerPlayer.setPlayerPoints("40");
@@ -73,5 +87,42 @@ public final class ScoreCalculationUtil {
     }
 
     private static void tiebreak(Player winnerPlayer, Player loosingPlayer) {
+        int winnerPlayerPoints = Integer.parseInt(winnerPlayer.getPlayerPoints());
+        int loosingPlayerPoints = Integer.parseInt(loosingPlayer.getPlayerPoints());
+
+        if (winnerPlayerPoints <= 6) {
+            winnerPlayer.setPlayerPoints(String.valueOf(winnerPlayerPoints + 1));
+            if (winnerPlayerPoints == 6 && winnerPlayerPoints - loosingPlayerPoints >= 1) {
+                setIsWon(winnerPlayer, loosingPlayer);
+            }
+        }
+
+        if (winnerPlayerPoints > 6) {
+            winnerPlayer.setPlayerPoints(String.valueOf(winnerPlayerPoints + 1));
+            if (winnerPlayerPoints - loosingPlayerPoints >= 1) {
+                setIsWon(winnerPlayer, loosingPlayer);
+            }
+        }
+    }
+
+    private static void setIsWon(Player winnerPlayer, Player loosingPlayer) {
+        clearPoints(winnerPlayer, loosingPlayer);
+        clearGames(winnerPlayer, loosingPlayer);
+        winnerPlayer.setPlayerSets(winnerPlayer.getPlayerSets() + 1);
+    }
+
+    private static void gameIsWon(Player winnerPlayer, Player loosingPlayer) {
+        clearPoints(winnerPlayer, loosingPlayer);
+        winnerPlayer.setPlayerGames(winnerPlayer.getPlayerGames() + 1);
+    }
+
+    private static void clearPoints(Player winnerPlayer, Player loosingPlayer) {
+        winnerPlayer.setPlayerPoints("0");
+        loosingPlayer.setPlayerPoints("0");
+    }
+
+    private static void clearGames(Player winnerPlayer, Player loosingPlayer) {
+        winnerPlayer.setPlayerGames(0);
+        loosingPlayer.setPlayerGames(0);
     }
 }
--- src/test/java/util/ScoreCalculationUtilTest.java
+++ src/test/java/util/ScoreCalculationUtilTest.java
@@ -2,116 +2,198 @@ package util;
 
 import model.Match;
 import model.Player;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
 
 public class ScoreCalculationUtilTest {
+
+    private Player firstPlayer;
+    private Player secondPlayer;
+    private Match match;
+
+    @BeforeEach
+    void setMatch() {
+        firstPlayer = new Player("Sergey");
+        firstPlayer.setId(1);
+        secondPlayer = new Player("Ilmira");
+        secondPlayer.setId(2);
+        match = new Match(firstPlayer, secondPlayer);
+    }
 
     @Nested
     @DisplayName("Points tests")
     class PointsTests {
+
         @Test
         void whenFirstPlayerHave0PointsAndWinPointThenPointsChangeTo15() {
-            Player firstPlayer = new Player("Sergey");
-            firstPlayer.setId(1);
-            Player secondPlayer = new Player("Ilmira");
-            secondPlayer.setId(2);
-
-            Match match = new Match(firstPlayer, secondPlayer);
             firstPlayer.setPlayerPoints("0");
             ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerPoints()).as("15");
+            Assertions.assertEquals("15", firstPlayer.getPlayerPoints());
+        }
+
+        @Test
+        void whenSecondPlayerHave0PointsAndWinPointThenPointsChangeTo15() {
+            firstPlayer.setPlayerPoints("0");
+            ScoreCalculationUtil.scoreCalculation(secondPlayer.getId(), match);
+            Assertions.assertEquals("15", secondPlayer.getPlayerPoints());
         }
 
         @Test
         void whenFirstPlayerHave15PointsAndWinPointThenPointsChangeTo30() {
-            Player firstPlayer = new Player("Sergey");
-            firstPlayer.setId(1);
-            Player secondPlayer = new Player("Ilmira");
-            secondPlayer.setId(2);
-
-            Match match = new Match(firstPlayer, secondPlayer);
             firstPlayer.setPlayerPoints("15");
             ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerPoints()).as("30");
+            Assertions.assertEquals("30", firstPlayer.getPlayerPoints());
         }
 
         @Test
         void whenFirstPlayerHave30PointsAndWinPointThenPointsChangeTo40() {
-            Player firstPlayer = new Player("Sergey");
-            firstPlayer.setId(1);
-            Player secondPlayer = new Player("Ilmira");
-            secondPlayer.setId(2);
-
-            Match match = new Match(firstPlayer, secondPlayer);
             firstPlayer.setPlayerPoints("30");
             ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerPoints()).as("40");
+            Assertions.assertEquals("40", firstPlayer.getPlayerPoints());
         }
 
         @Test
         void whenFirstPlayerHave40AndSecondPlayerHave40PointsAndFirstPlayerWinPointThenPointsChangeToAD() {
-            Player firstPlayer = new Player("Sergey");
-            firstPlayer.setId(1);
-            Player secondPlayer = new Player("Ilmira");
-            secondPlayer.setId(2);
-
-            Match match = new Match(firstPlayer, secondPlayer);
             firstPlayer.setPlayerPoints("40");
             secondPlayer.setPlayerPoints("40");
             ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerPoints()).as("AD");
+            Assertions.assertEquals("AD", firstPlayer.getPlayerPoints());
         }
 
         @Test
         void whenFirstPlayerHaveADAndFirstPlayerWinPointThenPointsChangeTo0AndFirstPlayerGamesChangeTo1AndPlayersPointsChangeTo0() {
-            Player firstPlayer = new Player("Sergey");
-            firstPlayer.setId(1);
-            Player secondPlayer = new Player("Ilmira");
-            secondPlayer.setId(2);
-
-            Match match = new Match(firstPlayer, secondPlayer);
             firstPlayer.setPlayerPoints("AD");
             secondPlayer.setPlayerPoints("");
             ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerPoints()).as("0");
-            org.assertj.core.api.Assertions.assertThat(secondPlayer.getPlayerPoints()).as("0");
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerGames()).as("1");
-            org.assertj.core.api.Assertions.assertThat(secondPlayer.getPlayerGames()).as("0");
+            Assertions.assertEquals("0", firstPlayer.getPlayerPoints());
+            Assertions.assertEquals("0", secondPlayer.getPlayerPoints());
+            Assertions.assertEquals(1, firstPlayer.getPlayerGames());
+            Assertions.assertEquals(0, secondPlayer.getPlayerGames());
         }
 
         @Test
         void whenFirstPlayerHaveADAndHaveGames1AndFirstPlayerWinPointThenPointsChangeTo0AndFirstPlayerGamesChangeTo2AndPlayersPointsChangeTo0() {
-            Player firstPlayer = new Player("Sergey");
-            firstPlayer.setId(1);
-            Player secondPlayer = new Player("Ilmira");
-            secondPlayer.setId(2);
-
-            Match match = new Match(firstPlayer, secondPlayer);
             firstPlayer.setPlayerPoints("AD");
             secondPlayer.setPlayerPoints("");
+            firstPlayer.setPlayerGames(1);
             ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerPoints()).as("0");
-            org.assertj.core.api.Assertions.assertThat(secondPlayer.getPlayerPoints()).as("0");
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerGames()).as("2");
-            org.assertj.core.api.Assertions.assertThat(secondPlayer.getPlayerGames()).as("0");
+            Assertions.assertEquals("0", firstPlayer.getPlayerPoints());
+            Assertions.assertEquals("0", secondPlayer.getPlayerPoints());
+            Assertions.assertEquals(2, firstPlayer.getPlayerGames());
+            Assertions.assertEquals(0, secondPlayer.getPlayerGames());
         }
 
         @Test
         void whenFirstPlayerHaveADAndSecondPlayerWinPointThenPointsChangeTo40() {
-            Player firstPlayer = new Player("Sergey");
-            firstPlayer.setId(1);
-            Player secondPlayer = new Player("Ilmira");
-            secondPlayer.setId(2);
-
-            Match match = new Match(firstPlayer, secondPlayer);
             firstPlayer.setPlayerPoints("AD");
             secondPlayer.setPlayerPoints("");
             ScoreCalculationUtil.scoreCalculation(secondPlayer.getId(), match);
-            org.assertj.core.api.Assertions.assertThat(firstPlayer.getPlayerPoints()).as("40");
-            org.assertj.core.api.Assertions.assertThat(secondPlayer.getPlayerPoints()).as("40");
+            Assertions.assertEquals("40", firstPlayer.getPlayerPoints());
+            Assertions.assertEquals("40", secondPlayer.getPlayerPoints());
+        }
+    }
+
+    @Nested
+    @DisplayName("GamesTests")
+    class GamesTests {
+
+        @BeforeEach
+        void setPoints() {
+            firstPlayer.setPlayerPoints("AD");
+            secondPlayer.setPlayerPoints("");
+        }
+
+        @Test
+        void whenFirstPlayerHaveADAndFirstPlayerWinPointThenPointsChangeTo0AndFirstPlayerGamesChangeTo1AndPlayersPointsChangeTo0() {
+            ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
+            Assertions.assertEquals("0", firstPlayer.getPlayerPoints());
+            Assertions.assertEquals("0", secondPlayer.getPlayerPoints());
+            Assertions.assertEquals(1, firstPlayer.getPlayerGames());
+            Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+        }
+
+        @Test
+        void whenFirstPlayerHave5Games0SetsAndWinGameAndSecondPlayerHave4GamesThenGamesChangeTo0AndFirstPlayerSetChangeTo1() {
+            firstPlayer.setPlayerGames(5);
+            secondPlayer.setPlayerGames(4);
+            firstPlayer.setPlayerSets(0);
+            ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
+            Assertions.assertEquals("0", firstPlayer.getPlayerPoints());
+            Assertions.assertEquals("0", secondPlayer.getPlayerPoints());
+            Assertions.assertEquals(0, firstPlayer.getPlayerGames());
+            Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+            Assertions.assertEquals(1, firstPlayer.getPlayerSets());
+            Assertions.assertEquals(0, secondPlayer.getPlayerSets());
+        }
+
+        @Test
+        void whenFirstPlayerHave6Games0SetsAndSecondPlayerHave5GamesAndFirstPlayerWinGameThenFirstPlayerSetChangeTo1() {
+            firstPlayer.setPlayerGames(6);
+            secondPlayer.setPlayerGames(5);
+            firstPlayer.setPlayerSets(0);
+            ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
+            Assertions.assertEquals("0", firstPlayer.getPlayerPoints());
+            Assertions.assertEquals("0", secondPlayer.getPlayerPoints());
+            Assertions.assertEquals(0, firstPlayer.getPlayerGames());
+            Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+            Assertions.assertEquals(1, firstPlayer.getPlayerSets());
+            Assertions.assertEquals(0, secondPlayer.getPlayerSets());
+        }
+
+        @Nested
+        class Tiebreak {
+            @BeforeEach
+            void setGames() {
+                firstPlayer.setPlayerGames(6);
+                secondPlayer.setPlayerGames(6);
+            }
+
+            @Test
+            void whenFirstPlayerHave0PointsAndFirstPlayerWinPointThenFirstPlayerPointChangeTo1() {
+                firstPlayer.setPlayerPoints("0");
+                secondPlayer.setPlayerPoints("0");
+                ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
+                Assertions.assertEquals("1", firstPlayer.getPlayerPoints());
+            }
+
+            @Test
+            void whenFirstPlayerHave6PointsAndSecondPlayerHave5PointsAndFirstPlayerWinPointThenFirstPlayerWinSet() {
+                firstPlayer.setPlayerPoints("6");
+                secondPlayer.setPlayerPoints("5");
+                firstPlayer.setPlayerSets(0);
+                ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
+                Assertions.assertEquals("0", firstPlayer.getPlayerPoints());
+                Assertions.assertEquals("0", secondPlayer.getPlayerPoints());
+                Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+                Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+                Assertions.assertEquals(1, firstPlayer.getPlayerSets());
+            }
+
+            @Test
+            void whenFirstPlayerHave6PointsAndSecondPlayerHave0PointsAndFirstPlayerWinPointThenFirstPlayerWinSet() {
+                firstPlayer.setPlayerPoints("6");
+                secondPlayer.setPlayerPoints("0");
+                firstPlayer.setPlayerSets(0);
+                ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
+                Assertions.assertEquals("0", firstPlayer.getPlayerPoints());
+                Assertions.assertEquals("0", secondPlayer.getPlayerPoints());
+                Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+                Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+                Assertions.assertEquals(1, firstPlayer.getPlayerSets());
+            }
+
+            @Test
+            void whenFirstPlayerHave20PointsAndSecondPlayerHave19PointsAndFirstPlayerWinPointThenFirstPlayerWinSet() {
+                firstPlayer.setPlayerPoints("20");
+                secondPlayer.setPlayerPoints("19");
+                firstPlayer.setPlayerSets(0);
+                ScoreCalculationUtil.scoreCalculation(firstPlayer.getId(), match);
+                Assertions.assertEquals("0", firstPlayer.getPlayerPoints());
+                Assertions.assertEquals("0", secondPlayer.getPlayerPoints());
+                Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+                Assertions.assertEquals(0, secondPlayer.getPlayerGames());
+                Assertions.assertEquals(1, firstPlayer.getPlayerSets());
+            }
         }
     }
 }
