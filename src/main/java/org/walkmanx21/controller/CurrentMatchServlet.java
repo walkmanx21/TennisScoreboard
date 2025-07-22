42aea00e18a6151b121019ee8faac2adbc782eba
walkmanx21 <walkmanx21@gmail.com>
2025-07-22 15:39:44 +0300

    правка опечатки

--- src/main/java/org/walkmanx21/controller/CurrentMatchServlet.java
+++ src/main/java/org/walkmanx21/controller/CurrentMatchServlet.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 @WebServlet("/match-score/*")
 public class CurrentMatchServlet extends HttpServlet {
 
-    private final MatchService matchService = MatchService.getInstance();
+    private static final MatchService MATCH_SERVICE = MatchService.getInstance();
     private static final MatchRepositoryService MATCH_REPOSITORY_SERVICE = MatchRepositoryService.getInstance();
 
     @Override
@@ -31,7 +31,7 @@ public class CurrentMatchServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String matchId = req.getParameter("uuid");
         Integer playerWinPointId = Integer.parseInt(req.getParameter("PlayerWin"));
-        Match match = matchService.matchPerformance(playerWinPointId, matchId);
+        Match match = MATCH_SERVICE.matchPerformance(playerWinPointId, matchId);
         SetAttributesUtil.setCurrentMatchAttributes(req, match);
 
         if (match.getStatus() == MatchStatusService.BEING_PLAYED) {
